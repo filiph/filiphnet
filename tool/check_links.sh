e01211de180d57936d5160a03ebe503fa59668a3
Filip Hracek <filip.hracek@gmail.com>
2017-06-05 16:35:52 -0700

    Add travis support

---
+++ tool/check_links.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+PORT=4001
+export CHECK_EXIT_CODE=0
+
+set -x
+firebase serve --port $PORT  > /dev/null &
+FBS_PID=$!
+
+sleep 4
+
+linkcheck :4001 | tee $TMP/linkcheck-log.txt
+
+set +x
+
+if ! grep '^\s*0 errors' $TMP/linkcheck-log.txt | wc -l > /dev/null; then
+  CHECK_EXIT_CODE=1
+fi
+
+kill $FBS_PID
+
+exit $CHECK_EXIT_CODE
