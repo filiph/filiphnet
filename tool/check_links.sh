19bc876556265990ab96d777c0ffeef3f39b83fc
Filip Hracek <filip.hracek@gmail.com>
2019-05-30 11:06:37 -0700

    Simplify linkchecking

--- tool/check_links.sh
+++ tool/check_links.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/env bash
 
-PORT=4001
-export CHECK_EXIT_CODE=0
-TMP=$HOME
+set -e
 
-set -x
-superstatic --port $PORT > /dev/null &
+# Serve web on localhost.
+superstatic --port 4001 > /dev/null &
 FBS_PID=$!
 
+# Wait for superstatic to start.
 sleep 4
 
-linkcheck :4001 -e | tee $TMP/linkcheck-log.txt
+# Check links.
+linkcheck :4001 -e
 
-set +x
-
-if ! grep '^\s*0 errors' $TMP/linkcheck-log.txt | wc -l > /dev/null; then
-  CHECK_EXIT_CODE=1
-fi
-
-kill $FBS_PID
-
-exit $CHECK_EXIT_CODE
+# Kill the localhost server.
+kill ${FBS_PID}
