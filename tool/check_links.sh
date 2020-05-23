48d0ef131fcc0f066169a3ded52282bf6b35bdf2
Filip Hracek <filip.hracek@gmail.com>
2020-05-22 19:42:42 -0700

    Try ignoring warnings from linkcheck

--- tool/check_links.sh
+++ tool/check_links.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 # Serve web on localhost.
 superstatic --port 4001 > /dev/null &
 FBS_PID=$!
@@ -11,6 +9,14 @@ sleep 4
 
 # Check links.
 linkcheck :4001 -e --skip-file tool/check_links_skip_file.txt
+LINKCHECK_EXIT_CODE=$?
 
 # Kill the localhost server.
 kill ${FBS_PID}
+
+if [ ${LINKCHECK_EXIT_CODE} -eq 1 ] then
+  echo "The linkcheck tool had some warnings. We won't fail the build for that."
+  exit 0
+else
+  exit ${LINKCHECK_EXIT_CODE}
+fi
